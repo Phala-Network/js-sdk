47c9a20e83c2b7e2b84966922c639b0334091492
lcbyfc <lcbyfc@gmail.com>
2022-12-29 20:09:24 +0800

    package

---
+++ packages/sdk/tsup.browser.ts
@@ -0,0 +1,19 @@
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
+// import nodePolyfills from 'rollup-plugin-node-polyfills';
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  esbuildPlugins: [NodeModulesPolyfillPlugin(), NodeGlobalsPolyfillPlugin({
+    buffer: true
+  })],
+  entry: ['src/index.ts'],
+  outDir: './browser',
+  dts: true,
+  format: ['cjs', 'esm'],
+  ignoreWatch: ['*.test.ts'],
+  target: 'node16',
+  clean: true,
+  platform: 'browser',
+  noExternal: ['crypto-browserify','protobufjs', 'randombytes']
+})
