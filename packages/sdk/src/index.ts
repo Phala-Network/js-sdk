6ce9d6035bb5828eff4810f8cd7ba91a828056bf
vabaly <love0and0life@gmail.com>
2023-02-06 18:31:34 +0800

    feat: export pruntime_rpc.PhactoryAPI as PhactoryAPI, https://github.com/Phala-Network/js-sdk/issues/26

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -1,4 +1,8 @@
+import { pruntime_rpc } from "./proto";
+
 export * from "./lib/types";
 export * from "./lib/hex";
 export * from "./create";
 export * from "./certificate";
+
+export const PhactoryAPI = pruntime_rpc.PhactoryAPI;
